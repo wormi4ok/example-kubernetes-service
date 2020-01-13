4a9a6a1cf68f9d604e20ecd787e4d188287edfbe
Stanislav Petrashov <s@petrashov.ru>
2020-01-13 20:11:25 +0100

    Create new go application

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wormi4ok/favorite-tree
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+const myFavouriteTree = "Birch"
+
+func main() {
+	fmt.Printf("My favorite tree is %s", myFavouriteTree)
+}
