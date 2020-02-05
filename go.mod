e19105f95e65cc4e6e66629db096293263c95019
Stanislav Petrashov <s@petrashov.ru>
2020-02-05 16:29:53 +0100

    Add application metrics

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/wormi4ok/favorite-tree
 
 go 1.13
+
+require (
+	contrib.go.opencensus.io/exporter/prometheus v0.1.0 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/prometheus/client_golang v1.4.0 // indirect
+	go.opencensus.io v0.22.3 // indirect
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5 // indirect
+	google.golang.org/genproto v0.0.0-20200205142000-a86caf926a67 // indirect
+)
--- main.go
+++ main.go
@@ -11,6 +11,11 @@ import (
 	"strings"
 	"syscall"
 	"time"
+
+	"contrib.go.opencensus.io/exporter/prometheus"
+	"go.opencensus.io/plugin/ochttp"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/tag"
 )
 
 const myFavouriteTree = "Birch"
@@ -19,12 +24,33 @@ const defaultPort = "8080"
 func main() {
 	port := portFromEnv()
 
-	http.HandleFunc("/tree", treeHandler(myFavouriteTree))
+	pe, err := prometheus.NewExporter(prometheus.Options{
+		Namespace: "",
+	})
+	if err != nil {
+		log.Fatalf("Failed to create the Prometheus stats exporter: %v", err)
+	}
+	view.Register(
+		ochttp.ServerRequestCountView,
+		ochttp.ServerLatencyView,
+		ochttp.ServerRequestCountByMethod,
+		&view.View{
+			Name:        "http_server_response_count_by_status_code",
+			Description: "Server response count by status code",
+			TagKeys:     []tag.Key{ochttp.StatusCode, ochttp.KeyServerRoute},
+			Measure:     ochttp.ServerLatency,
+			Aggregation: view.Count(),
+		},
+	)
+
+	http.Handle("/metrics", ochttp.WithRouteTag(pe, "/metrics"))
+	http.Handle("/tree", ochttp.WithRouteTag(treeHandler(myFavouriteTree), "/tree"))
 
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%s", port),
-		WriteTimeout: time.Second * 60,
+		Handler:      &ochttp.Handler{},
 		ReadTimeout:  time.Second * 15,
+		WriteTimeout: time.Second * 60,
 		IdleTimeout:  time.Second * 60,
 	}
 	log.Printf("Starting service on port %s...\n", port)
@@ -53,6 +79,7 @@ func treeHandler(favouriteTree string) http.HandlerFunc {
 		w.WriteHeader(200)
 		err := json.NewEncoder(w).Encode(response{MyFavouriteTree: favouriteTree})
 		if err != nil {
+			log.Printf("TreeHandler error: %v", err)
 			w.WriteHeader(500)
 		}
 	}
